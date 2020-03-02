0f12df52d54e36341a89d210c4b34a22c0f0ef71
puthiyajothy <saidputhi@gmail.com>
2020-03-02 10:37:12 +0530

    basic oauth

--- oauth-service/src/main/java/com/java/oauth/config/OauthConfig.java
+++ oauth-service/src/main/java/com/java/oauth/config/OauthConfig.java
@@ -1,7 +1,12 @@
 package com.java.oauth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
@@ -9,22 +14,32 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 
 @Configuration
 public class OauthConfig extends WebSecurityConfigurerAdapter implements AuthorizationServerConfigurer{
+	
+	@Autowired
+    AuthenticationManager authenticationManager;
+    PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+    
+    
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		// TODO Auto-generated method stub
+		security.checkTokenAccess("permitAll");
 		
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		
-		clients.inMemory().withClient(clientId:"web").secret("webpass").scopes("READ","WRITE").authorizedGrantTypes("password","authorization_code");
+        clients.inMemory().withClient("web").secret(passwordEncoder.encode("webpass")).scopes("READ", "WRITE").authorizedGrantTypes("password", "authorization_code");
+
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		// TODO Auto-generated method stub
+        endpoints.authenticationManager(authenticationManager);
 		
 	}
 
--- oauth-service/src/main/java/com/java/oauth/config/Userconfiguration.java
+++ oauth-service/src/main/java/com/java/oauth/config/Userconfiguration.java
@@ -3,15 +3,19 @@ package com.java.oauth.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class Userconfiguration extends GlobalAuthenticationConfigurerAdapter{
 
+    PasswordEncoder passwordEncoder= PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
 	@Override
 	public void init(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser(username:"jothi").password(12345).roles("USER","ADMIN","MANAGER").authorities("CAN_READ,CAN_WRITE","CAN_DELETE")
-		.withUser(username:"dilu").password("manager").roles("USER","ADMIN").authorities("CAN_READ,CAN_WRITE");
-		
-		super.init(auth);
+		auth.inMemoryAuthentication().withUser("jothi").password("1234").roles("USER","ADMIN","MANAGER")
+		.authorities("CAN_READ,CAN_WRITE","CAN_DELETE")
+		.and()
+		.withUser("dilu").password("manager").roles("USER","ADMIN").authorities("CAN_READ,CAN_WRITE");
 	}
 }
