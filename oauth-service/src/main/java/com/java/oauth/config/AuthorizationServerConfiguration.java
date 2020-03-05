7667a97539b6f8a879a6f1d7044a5503861298b5
puthiyajothy <saidputhi@gmail.com>
2020-03-05 10:23:54 +0530

    jdbc connetion completed

--- oauth-service/src/main/java/com/java/oauth/config/AuthorizationServerConfiguration.java
+++ oauth-service/src/main/java/com/java/oauth/config/AuthorizationServerConfiguration.java
@@ -3,6 +3,7 @@ package com.java.oauth.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,6 +11,8 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 @Configuration
 public class AuthorizationServerConfiguration  implements AuthorizationServerConfigurer{
@@ -23,6 +26,11 @@ public class AuthorizationServerConfiguration  implements AuthorizationServerCon
 	
 	@Autowired
 	AuthenticationManager authenticationManager;
+	
+	@Bean
+	TokenStore JdbcTokenStore() {
+		return new JdbcTokenStore(dataSource);
+	}
 	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -38,6 +46,7 @@ public class AuthorizationServerConfiguration  implements AuthorizationServerCon
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.tokenStore(JdbcTokenStore());
 		endpoints.authenticationManager(authenticationManager);		
 	}
 
